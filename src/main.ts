0892fb2f986022f86ca2a81ff02e198cfd3ea420
ikernm <ikernieblamoreno@hotmail.com>
2025-03-12 20:44:07 +0100

    turnos ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,10 @@
 import "./style.css";
 
-console.log("Hello Typescript!");
+const numeroTurno = document.getElementById(".numero-turno") as HTMLHeadingElement;
+const siguienteTurno = document.getElementById("siguiente-turno") as HTMLButtonElement;
+const anteriorTurno = document.getElementById("anterior-turno") as HTMLButtonElement;
+const reiniciarTurno = document.getElementById("reiniciar-turno") as HTMLButtonElement;
+const inputTurno = document.getElementById("input-turno") as HTMLInputElement;
+const agregarTurno = document.getElementById("agregar-turno") as HTMLButtonElement;
+
+let turno = 1;
